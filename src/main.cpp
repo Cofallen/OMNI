e7b5a2aa215c2407c9171d1a4b67c11a01d7cd76
Cofallen <1727334304@qq.com>
2024-04-13 20:41:16 +0800

    换了can1，2，具体云台没试

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,7 @@ int main()
     thread T_UART(YU_F_DBUS_THREAD, &YU_V_DBUS);
     thread T_CHASSIS(YU_F_THREAD_CHASSIS_MECANUM);
 //    thread T_MONITOR(YU_F_THREAD_MONITOR);
-    thread T_VOFA(YU_F_THREAD_VOFA);
+//    thread T_VOFA(YU_F_THREAD_VOFA);
 //    thread T_TEST(YU_F_THREAD_TEST);
 //    thread T_GIMBAL(YU_F_THREAD_GIMBAL);
 
@@ -49,7 +49,7 @@ int main()
     T_CHASSIS.join();
     T_UART.join();
 //    T_MONITOR.join();
-    T_VOFA.join();
+//    T_VOFA.join();
 //    T_GIMBAL.join();
 
 }
