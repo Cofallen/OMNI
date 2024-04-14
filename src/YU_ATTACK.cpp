80f202d9561f96010d7aa541bb50b409c09d4b73
Cofallen <1727334304@qq.com>
2024-04-14 16:04:48 +0800

    ATTACK框架

--- src/YU_ATTACK.cpp
+++ src/YU_ATTACK.cpp
@@ -7,11 +7,15 @@
 #include "YU_DEFINE.h"
 #include "YU_CONFIG.h"
 #include "YU_UART.h"
+#include "YU_MATH.h"
 
 struct
 {
-    int test;
-} YU_TYPEDEF_ATTACK;
+    int16_t G_CURRENT_MAX;
+    int16_t G_ANGLE[2];
+    int16_t G_ANGLE_ERROR;
+    struct timeval G_TIMEOUT_MAX;
+} YU_V_MONITOR_ATTACK{ };
 
 bool YU_F_ATTACK_INIT()
 {
@@ -28,6 +32,27 @@ float YU_F_ATTACK_FIRE_AIM(YU_TYPEDEF_MOTOR *MOTOR, YU_TYPEDEF_DBUS *DBUS, uint8
 
 void YU_F_ATTACK_CHECK(YU_TYPEDEF_MOTOR *MOTOR)
 {
+    // 我想根据电流异常，超时时间，角度变化来确定卡弹，待测试
+    YU_V_MONITOR_ATTACK.G_ANGLE[YU_D_LAST] = YU_V_MONITOR_ATTACK.G_ANGLE[YU_D_NOW];
+    YU_V_MONITOR_ATTACK.G_ANGLE[YU_D_NOW]  = MOTOR->DATA.ANGLE_NOW;
+
+    if (MOTOR->DATA.CURRENT >= YU_V_MONITOR_ATTACK.G_CURRENT_MAX)
+    {
+        // 异常处理
+    }
+
+    YU_V_MONITOR_ATTACK.G_ANGLE_ERROR = YU_D_MATH_ABS((YU_V_MONITOR_ATTACK.G_ANGLE[YU_D_NOW] - YU_V_MONITOR_ATTACK.G_ANGLE[YU_D_LAST]));
+
+    // 超时设置
+    YU_V_MONITOR_ATTACK.G_TIMEOUT_MAX.tv_usec = 10;
+
+    usleep(YU_V_MONITOR_ATTACK.G_TIMEOUT_MAX.tv_usec);
+    YU_V_MONITOR_ATTACK.G_ANGLE[YU_D_LAST] = YU_V_MONITOR_ATTACK.G_ANGLE[YU_D_NOW];
+
+    if (YU_V_MONITOR_ATTACK.G_ANGLE_ERROR < 600)
+    {
+        // 异常处理 倒转
+    }
 
 }
 
