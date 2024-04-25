b326143337f26f69bbb75c8bf958b45ecef7c180
Cofallen <1727334304@qq.com>
2024-04-25 18:10:51 +0800

    视觉未优化接收发送

--- inc/YU_VISION.h
+++ inc/YU_VISION.h
@@ -5,4 +5,42 @@
 #ifndef DEMO_YU_VISION_H
 #define DEMO_YU_VISION_H
 
+#include <cstdio>
+#include <cstdint>
+
+union YU_TYPEDEF_VISION_SEND
+{
+    struct
+    {
+        float PIT_DATA;
+        float YAW_DATA;
+        float INIT_FIRE_SPEED;
+        int FLAG;
+        bool COLOR;
+        unsigned int TIME;
+    }DATA;
+    uint8_t ALL[24];  // 标记，感觉是24
+};
+union YU_TYPEDEF_VISION_RECV
+{
+    struct
+    {
+        float PIT_DATA;
+        float YAW_DATA;
+        bool TARGET;
+    }DATA;
+    uint8_t ALL[9];
+};
+
+typedef struct
+{
+    union YU_TYPEDEF_VISION_RECV RECV;
+    union YU_TYPEDEF_VISION_SEND SEND;
+}YU_TYPEDEF_VISION;
+
+extern YU_TYPEDEF_VISION YU_V_VISION;
+
+[[noreturn]] void YU_F_THREAD_VISION(YU_TYPEDEF_VISION *VISION);
+
+
 #endif //DEMO_YU_VISION_H
--- src/YU_VISION.cpp
+++ src/YU_VISION.cpp
@@ -1,3 +1,66 @@
 //
 // Created by 17273 on 2024/4/24.
 //
+#include "YU_VISION.h"
+#include "YU_DEFINE.h"
+
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include "stdio.h"
+#include "string.h"
+#include <thread>
+
+YU_TYPEDEF_VISION YU_V_VISION;
+
+// 待优化接收发送,扔进小循环
+[[noreturn]] void YU_F_THREAD_VISION(YU_TYPEDEF_VISION *VISION)
+{
+    while (true)
+    {
+        static int SOCKET_FD;
+        static struct sockaddr_in SERVER_ADDR{ }, CLIENT_ADDR{ };
+        socklen_t CLIENT_ADDR_LEN = sizeof(struct sockaddr_in);
+
+        SOCKET_FD = socket(AF_INET, SOCK_STREAM, 0);
+        if (SOCKET_FD == -1)
+        {
+            perror("视觉SOCKET FD 创建失败\n");
+        }
+
+        memset(&SERVER_ADDR, 0, sizeof(SERVER_ADDR));
+        SERVER_ADDR.sin_family = AF_INET;
+        SERVER_ADDR.sin_port = htons(8080); // 随便给的
+        SERVER_ADDR.sin_addr.s_addr = htonl(INADDR_ANY);
+
+        if (bind(SOCKET_FD,(struct sockaddr *)&SERVER_ADDR,(socklen_t)sizeof(SERVER_ADDR)) < 0)
+        {
+            perror("视觉 bind 失败\n");
+        }
+        if (listen(SOCKET_FD, 5) == -1)
+        {
+            perror("视觉 Listen 失败\n");
+            close(SOCKET_FD);
+        }
+
+        if (accept(SOCKET_FD, (struct sockaddr *)&CLIENT_ADDR, &CLIENT_ADDR_LEN) < 0)
+        {
+            perror("视觉 accept 失败\n");
+            printf("尝试重连......\n");
+        }
+
+        // 接收，发送
+        if (recv(SOCKET_FD, &VISION->RECV, sizeof(VISION->RECV.ALL), 0) < 0)
+        {
+            perror("从视觉 recv 失败\n");
+            close(SOCKET_FD);
+            printf("尝试重连......\n");
+        }
+        if (send(SOCKET_FD, &VISION->SEND, sizeof(VISION->SEND.ALL), 0) < 0)
+        {
+            perror("从视觉 send 失败\n");
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));  // 延时0.2s
+    }
+}
